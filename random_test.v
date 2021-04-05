bf685cf1f86e7b84211b26d5e924467548c2dff5
Karthik Ramachandran <karthik.ramachandran@duke.edu>
2021-04-04 23:51:23 -0400

    block drops to bottom and stops

--- random_test.v
+++ random_test.v
@@ -1,15 +1,13 @@
-module random_test;
-    reg[4:0] rand;
-    integer i;
-    integer seed = 20;
-    initial begin    
-        for (i=0; i<15; i=i+1) begin
-            rand = $urandom(seed)%10;
-            $display("random from 0 to 10: %d",rand); 
-        end 
-        for (i=0; i<5; i=i+1) begin
-            rand = $urandom%2;
-            $display("random from 0 to 1: %d",rand); 
-        end 
+//CANNOT USE THIS - NOT SYNTHESIZABLE
+module random_test(a);
+    reg seed = 20;
+
+
+   output[10:0] a;
+   assign a = $urandom(20);
+   
+   initial begin
+       #20
+       $display("A1: %d", a);
    end
 endmodule
